7e51aef435912ae1fc2d5f0149b71ed9bf5b4182
Wolfyxon <wolfyxon@gmail.com>
2026-03-25 10:37:42 +0100

    Impl side menu opening

--- app/src/main/java/com/wolfyxon/firetell/android/ChatActivity.java
+++ app/src/main/java/com/wolfyxon/firetell/android/ChatActivity.java
@@ -2,12 +2,39 @@ package com.wolfyxon.firetell.android;
 
 import android.os.Bundle;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class ChatActivity extends AppCompatActivity {
+    DrawerLayout main;
+    NavigationView sideMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        main = findViewById(R.id.main);
+        sideMenu = findViewById(R.id.chat_menu);
+
+        findViewById(R.id.menu_btn).setOnClickListener(l -> {
+            openSideMenu();
+        });
+
+        OnBackPressedCallback backCallback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                openSideMenu();
+            }
+        };
+
+        getOnBackPressedDispatcher().addCallback(this, backCallback);
+    }
+
+    void openSideMenu() {
+        main.openDrawer(sideMenu);
     }
 }
